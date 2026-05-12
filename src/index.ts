2f9398ea373c791eded7ded6007c847b61437058
Marco Aurelio Hansen de Oliveira <ssnpett18@firjan.com.br>
2026-05-12 19:36:34 -0300

    adiciona a conexão ao banco

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco conectado!");
+    app.listen(process.env.PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log("Erro ao conectar no banco: ", error));
