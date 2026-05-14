cd8a55ff1bfd803505452b734a2c33e305bb27cc
Marco Aurelio Hansen de Oliveira <ssnpett18@firjan.com.br>
2026-05-13 21:32:01 -0300

    ajuste tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import type { Application } from "express";
+import { AppDataSource } from "./data-source.js";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
